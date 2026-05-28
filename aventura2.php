1d44d421e343f016340119271538298989b2deb1
Chrix19 <christopher.valle2027@adoc.superate.org.sv>
2026-05-28 16:58:51 -0600

    Buying process started

--- aventura2.php
+++ aventura2.php
@@ -1,19 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="aventura2.css">
+    <title>Mapa de Aventura</title>
+    <link rel="stylesheet" href="styles/aventura2.css">
 </head>
 <body>
-  <div class="mapa-contenedor">
-    <img src="images/fondo.png" alt="Mapa de Capibaras" class="mapa-fondo">
 
-    <a href="juegos.php" class="nivel-link nivel-1">
-        <span class="nivel-texto">Nivel 1</span>
-        <div class="estrellas">★★★</div>
-    </a>
-</div>
+    <div class="mapa-contenedor">
+        <img src="images/fondo.png" alt="Mapa de Capibaras" class="mapa-fondo">
+
+        <a href="juego_oraciones.php" class="boton-nivel nivel-1" title="Ir al Nivel 1">
+            <span class="etiqueta-nivel">Nivel</span>
+            <span class="numero-nivel">1</span>
+            <div class="estrellas">★★★</div>
+        </a>
+        
+        <a href="juegos.php?nivel=2" class="boton-nivel nivel-2" title="Ir al Nivel 2">
+            <span class="etiqueta-nivel">Nivel</span>
+            <span class="numero-nivel">2</span>
+            <div class="estrellas">★★★</div>
+        </a>
+        
+        <a href="juegos.php?nivel=3" class="boton-nivel nivel-3" title="Ir al Nivel 3">
+            <span class="etiqueta-nivel">Nivel</span>
+            <span class="numero-nivel">3</span>
+            <div class="estrellas">☆☆☆</div>
+        </a>
+        
+        <a href="juegos.php?nivel=4" class="boton-nivel nivel-4" title="Ir al Nivel 4">
+            <span class="etiqueta-nivel">Nivel</span>
+            <span class="numero-nivel">4</span>
+            <div class="estrellas">☆☆☆</div>
+        </a>
+        
+        <a href="juegos.php?nivel=5" class="boton-nivel nivel-5" title="Ir al Nivel 5">
+            <span class="etiqueta-nivel">Nivel</span>
+            <span class="numero-nivel">5</span>
+            <div class="estrellas">☆☆☆</div>
+        </a>
+    </div>
+
 </body>
 </html>
