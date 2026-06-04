b600f106c37af2e449227d6da729dce38926333d
Val279 <alisson.lopez2027@adoc.superate.org.sv>
2026-06-03 22:40:37 -0600

    new changes

--- index.php
+++ index.php
@@ -25,25 +25,17 @@
             </button>
 
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
-<<<<<<< HEAD
-    <ul class="navbar-nav">
-        <li class="nav-item">
-            <?php if(isset($_SESSION['userID'])): ?>
-                <a class="nav-link" href="tus-mascotas.php">Tus mascotas</a>
-            <?php else: ?>
-                <a class="nav-link" href="register.html">Tus mascotas</a>
-            <?php endif; ?>
-        </li>
-    </ul>
-</div>
-  
-=======
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="tus-mascotas.php">Tus Mascotas</a>
+                        <?php if(isset($_SESSION['userID'])): ?>
+                            <a class="nav-link" href="tus-mascotas.php">Tus mascotas</a>
+                        <?php else: ?>
+                            <a class="nav-link" href="register.html">Tus mascotas</a>
+                        <?php endif; ?>
                     </li>
-
->>>>>>> a91a5ea58fe957b61f3f4d2f33f049ca63eb630c
+                </ul>
+            </div>
+  
                     <li class="nav-item">
                         <a class="nav-link" href="sobre-nosotros.php">Sobre Nosotros</a>
                     </li>
