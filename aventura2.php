0b73033347094ce9d351bc8e4eb8f51cc6dbbaaa
gaab099 <maria.pineda2027@adoc.superate.org.sv>
2026-06-15 22:27:54 -0600

    Update

--- aventura2.php
+++ aventura2.php
@@ -8,37 +8,59 @@
 </head>
 <body>
 
+    <?php include 'navbar.php'; ?>
+
     <div class="mapa-contenedor">
         <img src="images/fondo.png" alt="Mapa de Capibaras" class="mapa-fondo">
 
         <a href="juego-palabras.php" class="boton-nivel nivel-1" title="Ir al Nivel 1">
-            <span class="etiqueta-nivel">Nivel</span>
-            <span class="numero-nivel">1</span>
-            <div class="estrellas">★★★</div>
+            <span class="texto-nivel">Nivel 1</span>
+            <div class="piedra-asset"></div>
+            <div class="estrellas">
+                <span class="estrella activa">★</span>
+                <span class="estrella activa">★</span>
+                <span class="estrella activa">★</span>
+            </div>
         </a>
         
         <a href="juegos.php?nivel=2" class="boton-nivel nivel-2" title="Ir al Nivel 2">
-            <span class="etiqueta-nivel">Nivel</span>
-            <span class="numero-nivel">2</span>
-            <div class="estrellas">★★★</div>
+            <span class="texto-nivel">Nivel 2</span>
+            <div class="piedra-asset"></div>
+            <div class="estrellas">
+                <span class="estrella activa">★</span>
+                <span class="estrella activa">★</span>
+                <span class="estrella activa">★</span>
+            </div>
         </a>
         
         <a href="juegos.php?nivel=3" class="boton-nivel nivel-3" title="Ir al Nivel 3">
-            <span class="etiqueta-nivel">Nivel</span>
-            <span class="numero-nivel">3</span>
-            <div class="estrellas">☆☆☆</div>
+            <span class="texto-nivel">Nivel 3</span>
+            <div class="piedra-asset"></div>
+            <div class="estrellas">
+                <span class="estrella activa">★</span>
+                <span class="estrella activa">★</span>
+                <span class="estrella activa">★</span>
+            </div>
         </a>
         
         <a href="juegos.php?nivel=4" class="boton-nivel nivel-4" title="Ir al Nivel 4">
-            <span class="etiqueta-nivel">Nivel</span>
-            <span class="numero-nivel">4</span>
-            <div class="estrellas">☆☆☆</div>
+            <span class="texto-nivel">Nivel 4</span>
+            <div class="piedra-asset"></div>
+            <div class="estrellas">
+                <span class="estrella activa">★</span>
+                <span class="estrella activa">★</span>
+                <span class="estrella activa">★</span>
+            </div>
         </a>
         
         <a href="juegos.php?nivel=5" class="boton-nivel nivel-5" title="Ir al Nivel 5">
-            <span class="etiqueta-nivel">Nivel</span>
-            <span class="numero-nivel">5</span>
-            <div class="estrellas">☆☆☆</div>
+            <span class="texto-nivel">Nivel 5</span>
+            <div class="piedra-asset"></div>
+            <div class="estrellas">
+                <span class="estrella activa">★</span>
+                <span class="estrella activa">★</span>
+                <span class="estrella activa">★</span>
+            </div>
         </a>
     </div>
 
